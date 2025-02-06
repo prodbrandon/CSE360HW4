6d091e0b108658f5d61b71862596f97a469a61a6
journeyhancock <journeyhancock480@gmail.com>
2025-02-05 23:19:40 -0700

    v1.06

---
+++ src/passwordEvaluationTestbed/PasswordEvaluationTestingAutomation.java
@@ -0,0 +1,144 @@
+package passwordEvaluationTestbed;
+
+/*******
+ * <p> Title: PasswordEvaluationTestingAutomation Class. </p>
+ * 
+ * <p> Description: A Java demonstration for semi-automated tests </p>
+ * 
+ * <p> Copyright: Lynn Robert Carter © 2022 </p>
+ * 
+ * @author Lynn Robert Carter
+ * 
+ * @version 1.00	2022-02-25 A set of semi-automated test cases
+ * @version 2.00	2024-09-22 Updated for use at ASU
+ * 
+ */
+public class PasswordEvaluationTestingAutomation {
+	
+	static int numPassed = 0;	// Counter of the number of passed tests
+	static int numFailed = 0;	// Counter of the number of failed tests
+
+	/*
+	 * This mainline displays a header to the console, performs a sequence of
+	 * test cases, and then displays a footer with a summary of the results
+	 */
+	public static void main(String[] args) {
+		/************** Test cases semi-automation report header **************/
+		System.out.println("______________________________________");
+		System.out.println("\nTesting Automation");
+
+		/************** Start of the test cases **************/
+		// should pass because it meets all reqs
+		performTestCase(1, "Aa!15678", true);
+		// should fail because it does not meet reqs and is not at least 8 char
+		performTestCase(2, "pas", false);
+		// should pass because it meets all reqs (uppercase, lowercase, number, special char, and at least 8 char)
+		performTestCase(3, "College_1", true);
+		// fails because it does not meet all reqs(missing uppercase, missing number, missing special char)
+		performTestCase(4, "password", false);
+		// fails because no password was entered
+		performTestCase(5, "", false);
+		// fails because password is missing uppercase and lowercase letters, and missing special char
+		performTestCase(6, "12345", false);
+		// passes because password meets all reqs
+		performTestCase(7, "023colL!", true);
+		// fails because password is missing number and special char
+		performTestCase(8, "Password", false);
+		// fails because password is missing special char
+		performTestCase(9, "Password123", false);
+		// fails because password is missing number and is not at least 8 char
+		performTestCase(10, "Pass!", false);
+		
+		/************** End of the test cases **************/
+		
+		/************** Test cases semi-automation report footer **************/
+		System.out.println("____________________________________________________________________________");
+		System.out.println();
+		System.out.println("Number of tests passed: "+ numPassed);
+		System.out.println("Number of tests failed: "+ numFailed);
+	}
+	
+	/*
+	 * This method sets up the input value for the test from the input parameters,
+	 * displays test execution information, invokes precisely the same recognizer
+	 * that the interactive JavaFX mainline uses, interprets the returned value,
+	 * and displays the interpreted result.
+	 */
+	private static void performTestCase(int testCase, String inputText, boolean expectedPass) {
+		/************** Display an individual test case header **************/
+		System.out.println("____________________________________________________________________________\n\nTest case: " + testCase);
+		System.out.println("Input: \"" + inputText + "\"");
+		System.out.println("______________");
+		System.out.println("\nFinite state machine execution trace:");
+		
+		/************** Call the recognizer to process the input **************/
+		String resultText= PasswordEvaluator.evaluatePassword(inputText);
+		
+		/************** Interpret the result and display that interpreted information **************/
+		System.out.println();
+		
+		// If the resulting text is empty, the recognizer accepted the input
+		if (resultText != "") {
+			 // If the test case expected the test to pass then this is a failure
+			if (expectedPass) {
+				System.out.println("***Failure*** The password <" + inputText + "> is invalid." + 
+						"\nBut it was supposed to be valid, so this is a failure!\n");
+				System.out.println("Error message: " + resultText);
+				numFailed++;
+			}
+			// If the test case expected the test to fail then this is a success
+			else {			
+				System.out.println("***Success*** The password <" + inputText + "> is invalid." + 
+						"\nBut it was supposed to be invalid, so this is a pass!\n");
+				System.out.println("Error message: " + resultText);
+				numPassed++;
+			}
+		}
+		
+		// If the resulting text is empty, the recognizer accepted the input
+		else {	
+			// If the test case expected the test to pass then this is a success
+			if (expectedPass) {	
+				System.out.println("***Success*** The password <" + inputText + 
+						"> is valid, so this is a pass!");
+				numPassed++;
+			}
+			// If the test case expected the test to fail then this is a failure
+			else {
+				System.out.println("***Failure*** The password <" + inputText + 
+						"> was judged as valid" + 
+						"\nBut it was supposed to be invalid, so this is a failure!");
+				numFailed++;
+			}
+		}
+		displayEvaluation();
+	}
+	
+	private static void displayEvaluation() {
+		if (PasswordEvaluator.foundUpperCase)
+			System.out.println("At least one upper case letter - Satisfied");
+		else
+			System.out.println("At least one upper case letter - Not Satisfied");
+
+		if (PasswordEvaluator.foundLowerCase)
+			System.out.println("At least one lower case letter - Satisfied");
+		else
+			System.out.println("At least one lower case letter - Not Satisfied");
+	
+
+		if (PasswordEvaluator.foundNumericDigit)
+			System.out.println("At least one digit - Satisfied");
+		else
+			System.out.println("At least one digit - Not Satisfied");
+
+		if (PasswordEvaluator.foundSpecialChar)
+			System.out.println("At least one special character - Satisfied");
+		else
+			System.out.println("At least one special character - Not Satisfied");
+
+		if (PasswordEvaluator.foundLongEnough)
+			System.out.println("At least 8 characters - Satisfied");
+		else
+			System.out.println("At least 8 characters - Not Satisfied");
+	}
+}
